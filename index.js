3f1df2e082203a1162b20bb03d774b8469abec3c
Sidharth Surya <imsidharthsurya@gmail.com>
2023-09-30 23:34:43 +0530

    try useState

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React,{useState} from "react";
 import ReactDOM from "react-dom/client";
-const myName="sidharth surya"
-const head1=<h1>this is {myName}</h1>//React element
+// const myName="sidharth surya"
+// const head1=<h1>this is {myName}</h1>//React element
 
-//React component   nothing but normal fn. which will return some piece of jsx or react element
-const HeaderComponenet=()=>{
+// //React component   nothing but normal fn. which will return some piece of jsx or react element
+// const HeaderComponenet=()=>{
+//     return (
+//         <div>
+//             {head1}
+//             <p>Made by learning react</p>
+//             <FooterComp/>
+//         </div>
+//     )
+// }
+// const FooterComp=()=>{
+//     return (
+//         <div>
+//             <p>all copyrights reserved</p>
+//         </div>
+//     )
+// }
+
+// console.log(head1);
+
+
+const FormComponent=()=>{
+    const [searchText,setSearchText]=useState("false")
     return (
-        <div>
-            {head1}
-            <p>Made by learning react</p>
-            <FooterComp/>
-        </div>
+        <>
+            <h1>{searchText}</h1>
+            <button onClick={()=>{
+                if(searchText==="false")
+                setSearchText("true")
+            else
+                setSearchText("false")
+            }}>Click Me</button>
+        </>
     )
 }
-const FooterComp=()=>{
-    return (
-        <div>
-            <p>all copyrights reserved</p>
-        </div>
-    )
-}
-
-console.log(head1);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponenet />)
+root.render(<FormComponent />)
